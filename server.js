250d21dde1d1377590c04f09742fbdf8628432c5
Cinnamon-Yoshi <alexbotham@gmail.com>
2026-07-29 19:11:29 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 const HOST_PIN = process.env.HOST_PIN || '8888';
-const VERSION = '3.10';
+const VERSION = '3.11';
 const LAST_UPDATED = 'July 2025';
 
 const fs = require('fs');
